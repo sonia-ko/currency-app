5aeb1f5b70f779eccaffeaf6234c250e758af440
Sonia Kozytska <sonya-ko@outlook.com>
2022-05-02 14:04:13 +0300

    fix bug

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Form.module.css";
-import {FocusEvent} from 'react'
 
 const Form: React.FC = () => {
   const [currency1, setCurrency1] = useState("usd");
@@ -16,9 +15,9 @@ const Form: React.FC = () => {
     const fetchData = async () => {
       if (!currenciesFetched) {
         const response = await fetch(
-          amountToUpdate === "amount1"
-            ? `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency2}/${currency1}.json`
-            : `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency1}/${currency2}.json`
+            amountToUpdate === "amount1"
+                ? `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency2}/${currency1}.json`
+                : `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency1}/${currency2}.json`
         );
         const necessaryAmount = await response.json();
         if (amountToUpdate === "amount1") {
@@ -48,72 +47,69 @@ const Form: React.FC = () => {
     currency2,
   ]);
 
-  const selectTextWhenFocus = (event: FocusEvent<HTMLInputElement>) => event.target.select();
-
   return (
-    <div className={styles.container}>
-      <div className={styles.formContainer}>
-        <div className={styles.currencyOption}>
-          <label htmlFor="number-currency-1">Currency 1</label>
-          <div>
-            <input
-              min={0}
-              onFocus={selectTextWhenFocus}
-              onChange={(event) => {
-                setAmount1(Number(event.target.value));
-                setAmountToUpdate("amount2");
-                setCurrenciesFetched(false);
-              }}
-              className={styles.input}
-              id="number-currency-1"
-              type="number"
-              value={amount1}
-            />
-            <select
-              className={styles.select}
-              onChange={(event) => {
-                setCurrency1(event.target.value);
-                setCurrenciesFetched(false);
-              }}
-            >
-              <option value="usd">USD</option>
-              <option value="uah">UAH</option>
-              <option value="eur">EUR</option>
-            </select>
+      <div className={styles.container}>
+        <div className={styles.formContainer}>
+          <div className={styles.currencyOption}>
+            <label htmlFor="number-currency-1">Currency 1</label>
+            <div>
+              <input
+                  min={0}
+                  onChange={(event) => {
+                    setAmount1(Number(event.target.value));
+                    setAmountToUpdate("amount2");
+                    setCurrenciesFetched(false);
+                  }}
+                  className={styles.input}
+                  id="number-currency-1"
+                  type="number"
+                  value={Number(amount1).toString()}
+              />
+              <select
+                  className={styles.select}
+                  onChange={(event) => {
+                    setCurrency1(event.target.value);
+                    setCurrenciesFetched(false);
+                  }}
+              >
+                <option value="usd">USD</option>
+                <option value="uah">UAH</option>
+                <option value="eur">EUR</option>
+              </select>
+            </div>
           </div>
-        </div>
-        <div>to: </div>
-        <div className={styles.currencyOption}>
-          <label htmlFor="number-currency-2">Currency 2</label>
-          <div>
-            <input
-              onChange={(event) => {
-                setAmount2(Number(event.target.value));
-                setAmountToUpdate("amount1");
-                setCurrenciesFetched(false);
-              }}
-              className={styles.input}
-              id="number-currency-2"
-              type="number"
-              min={0}
-              value={amount2}
-              onFocus={selectTextWhenFocus}
-            />
-            <select
-              className={styles.select}
-              onChange={(event) => {
-                setCurrency2(event.target.value);
-                setCurrenciesFetched(false);
-              }}
-            >
-              <option value="uah">UAH</option>
-              <option value="usd">USD</option>
-              <option value="eur">EUR</option>
-            </select>
+          <div>to: </div>
+          <div className={styles.currencyOption}>
+            <label htmlFor="number-currency-2">Currency 2</label>
+            <div>
+              <input
+                  onChange={(event) => {
+                    setAmount2(Number(event.target.value));
+                    setAmountToUpdate("amount1");
+                    setCurrenciesFetched(false);
+                  }}
+                  className={styles.input}
+                  id="number-currency-2"
+                  type="number"
+                  min={0}
+                  value={Number(amount2).toString()}
+              />
+              <select
+                  className={styles.select}
+                  onChange={(event) => {
+                    setCurrency2(event.target.value);
+
+                    setCurrenciesFetched(false);
+                  }}
+              >
+                <option value="uah">UAH</option>
+                <option value="usd">USD</option>
+                <option value="eur">EUR</option>
+              </select>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
